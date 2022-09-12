416117b1b3fe454bf577b396e681cd70ff9d2ac7
gim-yeonsu <seoburi@github.com>
2022-09-12 11:23:15 +0900

    gitignore등

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableJpaAuditing //Jpa Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
